f15441ed64a9b2642517bcddeac7a61cc2068020
x00jmx <jman.21estudos@gmail.com>
2024-04-08 09:13:22 -0300

    teste

---
+++ main.py 2.py
@@ -0,0 +1,14 @@
+class Aluno():
+    def __init__(self, matricula, nome, idade, sexo):
+        self.matricula = matricula
+        self.nome = nome
+        self.idade = idade
+        self.sexxo = sexo
+        
+aluno1 = Aluno(1231, 'Joao', 17, 'masculino')
+print('Matricula:', aluno1.matricula)
+print('')
+print('')
+print('')
+
+        
